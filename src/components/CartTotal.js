f78095f69f367b258c2140f0f070a36c896b71a2
amanc1248 <amanc1248@gmail.com>
2021-01-31 22:38:44 +0545

    ADD: CartTotal htm style

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -1,7 +1,15 @@
 import React from "react";
-import "./CartTotal.css"
+import "./CartTotal.css";
 function CartTotal() {
-  return <div className="cartTotal"></div>;
+  return (
+    <div className="cartTotal">
+      <h3>
+        Subtotal(5 items)
+        <span className="cart-total-price">$234.00</span>
+      </h3>
+      <button>Proceed To Checkout</button>
+    </div>
+  );
 }
 
 export default CartTotal;
