c8280fad8ad233c9f6db285f5f02de60041022c8
amanc1248 <amanc1248@gmail.com>
2021-02-01 22:05:40 +0545

    ADD: Made cart mobile responsive

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Header from "./components/Header";
 import CartItems from "./components/CartItems";
 import CartTotal from "./components/CartTotal";
 import data from "./Data";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 function App() {
   const [cartItems, setCartItems] = useState(data);
 
   return (
     <div className="App">
       <Header title="Amazon Cart"></Header>
-      <div className="App_main">
-        <CartItems items={cartItems} setCartItems ={setCartItems}></CartItems>
-        <CartTotal items={cartItems}></CartTotal>
+      <div className="App_main row no-gutters">
+        <div className="col-lg-8 col-12">
+          <CartItems items={cartItems} setCartItems={setCartItems}></CartItems>
+        </div>
+
+        <div className="col-lg-4 col-12">
+          <CartTotal items={cartItems}></CartTotal>
+        </div>
       </div>
     </div>
   );
